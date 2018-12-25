1c8c15cbd460336b3bfb457766ad1857d8988d95
Satinder Kaur <sandykaur2008@gmail.com>
2018-12-25 12:00:16 -0500

    working on stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,43 @@
 import React, { Component } from 'react';
+import axios from "axios"; 
 import './App.css';
 
+
 class App extends Component {
-  // Initialize state
-  state = { passwords: [] }
+  constructor() {
+    super();
+    this.state = {
+      message: null
+    }; 
+    this.componentDidMount = this.componentDidMount.bind(this); 
+  }
 
   // Fetch passwords after first mount
   componentDidMount() {
     this.getPasswords();
   }
 
-  getPasswords = () => {
+  getPasswords() {
     // Get the passwords and store them in state
-    fetch('/api/passwords')
-      .then(res => res.json())
-      .then(passwords => this.setState({ passwords }));
+    axios.get('/api/passwords')
+      .then(response => this.setState({ message: response.data.message }));
   }
 
   render() {
-    const { passwords } = this.state;
 
     return (
       <div className="App">
         {/* Render the passwords if we have them */}
-        {passwords.length ? (
           <div>
             <h1>5 Passwords.</h1>
-            <ul className="passwords">
               {/*
                 Generally it's bad to use "index" as a key.
                 It's ok for this example because there will always
                 be the same number of passwords, and they never
                 change positions in the array.
               */}
-              {passwords.map((password, index) =>
-                <li key={index}>
-                  {password}
-                </li>
-              )}
-            </ul>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Get More
-            </button>
+              <p>{this.state.message}</p> 
           </div>
-        ) : (
-          // Render a helpful message otherwise
-          <div>
-            <h1>No passwords :(</h1>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Try Again?
-            </button>
-          </div>
-        )}
       </div>
     );
   }
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const generatePassword = require('password-generator');
 
 const app = express();
 
@@ -9,17 +8,8 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
 app.get('/api/passwords', (req, res) => {
-  const count = 5;
-
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
-
   // Return them as json
-  res.json(passwords);
-
-  console.log(`Sent ${count} passwords`);
+  res.json({message: "hello"});
 });
 
 // The "catchall" handler: for any request that doesn't
